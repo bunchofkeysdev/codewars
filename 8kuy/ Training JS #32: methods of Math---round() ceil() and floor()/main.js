e3d70329b1a33bc193ef48d81d4fc3914d3d2b40
bunchofkeysdev <bunchofkeysdev@gmail.com>
2021-07-06 00:24:52 +0100

    codewars

--- 8kuy/ Training JS #32: methods of Math---round() ceil() and floor()/main.js
+++ 8kuy/ Training JS #32: methods of Math---round() ceil() and floor()/main.js
@@ -7,3 +7,8 @@ function roundIt(n) {
 
 
 }
+// shorter
+function roundIt(n) {
+    var [a, b] = n.toString().split('.');
+    return a.length > b.length ? Math.floor(n) : a.length === b.length ? Math.round(n) : Math.ceil(n);
+}
