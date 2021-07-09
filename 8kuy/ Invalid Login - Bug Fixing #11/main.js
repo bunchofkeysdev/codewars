351ee2ae14c856f15dbd9f68922d27f958a525ba
bunchofkeysdev <bunchofkeysdev@gmail.com>
2021-07-10 00:02:37 +0100

    codewars

---
+++ 8kuy/ Invalid Login - Bug Fixing #11/main.js
@@ -0,0 +1,5 @@
+function validate(username, password) {
+    if (password.includes('//') || password.includes('||')) return 'Wrong username or password!'
+    var database = new Database();
+    return database.login(username, password);
+}
