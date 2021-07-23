64463a8b2cc977cb98712d3b286a29d4d2495869
bunchofkeysdev <bunchofkeysdev@gmail.com>
2021-07-23 21:11:53 +0100

    codewars

--- 8kuy/Pythagorean Triple/main.js
+++ 8kuy/Pythagorean Triple/main.js
@@ -1,4 +1,4 @@
 function isPythagoreanTriple(integers) {
     integers.sort((a, b) => b - a)
-    return Math.pow(integers[0], 2) == Math.pow(integers[1], 2) + Math.pow(integers[2], 2) ? true : false
+    return Math.pow(integers[0], 2) == Math.pow(integers[1], 2) + Math.pow(integers[2], 2)
 }
