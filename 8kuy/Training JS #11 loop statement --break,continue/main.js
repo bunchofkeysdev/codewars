ef3422452cc4b02a5cf159ee69e8fd4bcaa735eb
bunchofkeysdev <bunchofkeysdev@gmail.com>
2021-08-06 03:17:13 +0100

    codewars

---
+++ 8kuy/Training JS #11 loop statement --break,continue/main.js
@@ -0,0 +1,13 @@
+function grabDoll(dolls) {
+    var bag = [];
+    //coding here
+    for (let i = 0; i < dolls.length; i++) {
+        if (bag.length === 3) break;
+        else if (dolls[i] === 'Hello Kitty' || dolls[i] === 'Barbie doll') {
+            bag.push(dolls[i])
+            continue;
+        }
+
+    }
+    return bag;
+}
