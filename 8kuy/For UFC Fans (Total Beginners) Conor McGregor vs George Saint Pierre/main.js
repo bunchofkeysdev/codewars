ef7957176864af06fd44510220972fdd2a059e02
bunchofkeysdev <bunchofkeysdev@gmail.com>
2021-09-27 14:33:31 +0100

    codewars

---
+++ 8kuy/For UFC Fans (Total Beginners) Conor McGregor vs George Saint Pierre/main.js
@@ -0,0 +1,5 @@
+var quote = function (fighter) {
+    // your code here
+    fighter = fighter.toLowerCase()
+    return fighter === 'conor mcgregor' ? "I'd like to take this chance to apologize.. To absolutely NOBODY!" : "I am not impressed by your performance."
+};
