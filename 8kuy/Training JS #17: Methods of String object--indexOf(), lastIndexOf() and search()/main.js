16b26d392509da314cdae545f376e5fc485a2e3c
bunchofkeysdev <bunchofkeysdev@gmail.com>
2021-10-11 18:09:38 +0100

    codewars

--- 8kuy/Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()/main.js
+++ 8kuy/Training JS #17: Methods of String object--indexOf(), lastIndexOf() and search()/main.js
@@ -2,3 +2,9 @@ function firstToLast(str, c) {
     return str.search(c) === -1 ? -1 : str.lastIndexOf(c) - str.indexOf(c)
 
 }
+
+// second
+function firstToLast(str, c) {
+    var first = str.indexOf(c), last = str.lastIndexOf(c);
+    return first == -1 ? -1 : last - first;
+}
