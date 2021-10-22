c2b7bdc5f743378282027e47c10deaad4ce6f041
bunchofkeysdev <bunchofkeysdev@gmail.com>
2021-10-22 23:41:52 +0100

    codewars

--- 8kuy/ Aspect Ratio Cropping - Part 1/main.js
+++ 8kuy/ Aspect Ratio Cropping - Part 1/main.js
@@ -1,3 +1,14 @@
 function aspectRatio(x, y) {
     return [Math.ceil(y * (16 / 9)), y]
 }
+
+// longer
+
+function aspectRatio(x, y) {
+    let result = [];
+
+    x = Math.ceil(y * 1.77777777);
+
+    result.push(x, y);
+    return result;
+}
