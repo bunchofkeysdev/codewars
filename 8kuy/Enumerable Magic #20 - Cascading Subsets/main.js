de5cd9c9837da50e6c0960f6eacc13acb38c0cf3
bunchofkeysdev <bunchofkeysdev@gmail.com>
2021-11-11 20:00:54 +0100

    codewars

--- 8kuy/Enumerable Magic #20 - Cascading Subsets/main.js
+++ 8kuy/Enumerable Magic #20 - Cascading Subsets/main.js
@@ -2,3 +2,15 @@ function eachCons(array, n) {
     return array.map((e, i) => array.slice(i, i + n)).filter(e => e.length == n)
 
 }
+
+// second
+
+function eachCons(array, n) {
+    let res = [];
+
+    for (let i = 0; i < array.length; i++) {
+        res.push(array.slice(i, i + n));
+    }
+
+    return res.filter(e => e.length === n);
+}
