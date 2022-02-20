084b9fe63fd8791953e1193e4e68f4d87c3ffe0e
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-02-20 16:53:11 +0100

    codewars

--- 7kuy/Sum of Minimums!/main.js
+++ 7kuy/Sum of Minimums!/main.js
@@ -1,1 +1,6 @@
 const sumOfMinimums = arr => arr.map(e => Math.min(...e)).reduce((a, b) => a + b, 0)
+
+// shorter
+function sumOfMinimums(arr) {
+    return arr.reduce((a, b) => a + Math.min(...b), 0);
+}
