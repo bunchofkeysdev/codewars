2bbf0dec95a83990073d9449f84c5f3c7832274d
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-03-04 16:37:53 +0100

    codewars

---
+++ 7kuy/Row Weights/main.js
@@ -0,0 +1,16 @@
+function rowWeights(array) {
+    let newArray = [0, 0]
+    if (array.length === 1) {
+        return [array[0], 0]
+    } else if (array.length === 2) {
+        return array
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            if (i % 2 === 0) {
+                newArray[0] += array[i]
+            } else {
+                newArray[1] += array[i]
+            }
+        } return newArray
+    }
+}
