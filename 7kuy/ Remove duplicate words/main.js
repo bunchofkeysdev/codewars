5f7d2b7d1940267b925efe362dc5bdff597932d9
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-03-13 16:40:23 +0100

    codewars

--- 7kuy/ Remove duplicate words/main.js
+++ 7kuy/ Remove duplicate words/main.js
@@ -2,4 +2,4 @@ const removeDuplicateWords = s => Array.from(new Set(s.split(' '))).join(' ')
 
 // shorter
 
-const removeDuplicateWords = s => [...new Set(s.split(' '))].join(' ')
+const removeDuplicateWords2 = s => [...new Set(s.split(' '))].join(' ')
