dedf1c6cb101a02b3d8fc7c0a91986b013498342
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-04-25 23:46:55 +0100

    codewars

---
+++ 8kuy/Cat years, Dog years/main.js
@@ -0,0 +1,43 @@
+var humanYearsCatYearsDogYears = function (humanYears) {
+
+    let catYears = 0
+    let dogYears = 0
+    if (humanYears === 1) {
+        catYears = 15
+        dogYears = 15
+    } else if (humanYears === 2) {
+        catYears = 15 + 9
+        dogYears = 15 + 9
+    } else {
+        catYears = 15 + 9 + ((humanYears - 2) * 4)
+        dogYears = 15 + 9 + ((humanYears - 2) * 5)
+
+
+    }
+
+    return [humanYears, catYears, dogYears]
+}
+
+//   second
+
+var humanYearsCatYearsDogYears = function (humanYears) {
+    let arr = []
+    let catYears = 0
+    let dogYears = 0
+    if (humanYears === 1) {
+        catYears = 15
+        dogYears = 15
+    } else if (humanYears === 2) {
+        catYears = 15 + 9
+        dogYears = 15 + 9
+    } else {
+        catYears = 15 + 9 + ((humanYears - 2) * 4)
+        dogYears = 15 + 9 + ((humanYears - 2) * 5)
+
+
+    }
+    arr.push(humanYears)
+    arr.push(catYears)
+    arr.push(dogYears)
+    return arr
+}
