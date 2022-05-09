0ec6fe5d1c028e615f742f0e51523b94cb0ebfc2
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-05-09 16:29:29 +0100

    codewars

---
+++ smallestIntegerInArray/main.js
@@ -0,0 +1,15 @@
+class SmallestIntegerFinder {
+    findSmallestInt(args) {
+        return Math.min(...args)
+
+        //   let smallest = args[0]
+        //   for (let i = 0; i < args.length; i++){
+        //     if(args[i]< smallest){
+        //       smallest = args[i]
+        //     }
+        //   }
+        //   return smallest
+
+
+    }
+}
