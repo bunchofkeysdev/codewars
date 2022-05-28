45a032da888ac9ddca6d13d9149ab479a1de4b03
Isiavwe Juliet Oghenerukevwe <49438509+bunchofkeysdev@users.noreply.github.com>
2022-05-28 19:33:38 +0300

    Delete howGoodAreYouReally directory

--- howGoodAreYouReally/main.js
+++
@@ -1,4 +0,0 @@
-function betterThanAverage(classPoints, yourPoints) {
-    let total = classPoints.reduce((a, b) => a + b, 0) + yourPoints
-    return total / (classPoints.length + 1) < yourPoints ? true : false
-}
