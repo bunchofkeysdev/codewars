3649c388f605a77fda18a3fb279307329d3c4336
Isiavwe Juliet Oghenerukevwe <49438509+bunchofkeysdev@users.noreply.github.com>
2022-05-28 19:41:39 +0300

    Delete secretMessage directory

--- secretMessage/main.js
+++
@@ -1,8 +0,0 @@
-function greet(name) {
-    //   return "Hello, " + name + "!";
-    if (name === "Johnny") {
-        return "Hello, my love!";
-    } else {
-        return "Hello, " + name + "!";
-    }
-}
