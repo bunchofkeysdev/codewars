46759b5f000bea6aa4c7a25d32de3bc00ad14f1a
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-07-07 02:16:56 +0100

    codewars

--- 8kuy/Training JS #18 Methods of String object--concat() split() and its good friend join()/main.js
+++ 8kuy/Training JS #18 Methods of String object--concat() split() and its good friend join()/main.js
@@ -1,3 +1,17 @@
 function splitAndMerge(string, separator) {
     return string.split(' ').map(e => e.split("").join(separator)).join(" ")
 }
+
+// second
+
+function splitAndMerge(str, sp) {
+    var words = str.split(" ");
+    var arr = [];
+    var res = []
+    for (i = 0; i < words.length; i++) {
+        arr.push(words[i].split("").join(sp));
+    }
+    res = arr.join(" ");
+    return res;
+
+}
