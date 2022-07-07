2aaedc953ae0065a5fa693aaed90f2c8a7c1f5d0
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-07-07 01:58:48 +0100

    codewars

--- 8kuy/Grasshopper - Terminal Game #1/main.js
+++ 8kuy/Grasshopper - Terminal Game #1/main.js
@@ -8,3 +8,13 @@ class Hero {
     }
 
 }
+
+// second
+
+function Hero(name) {
+    this.name = name || 'Hero';
+    this.position = '00';
+    this.health = 100;
+    this.damage = 5;
+    this.experience = 0;
+}
