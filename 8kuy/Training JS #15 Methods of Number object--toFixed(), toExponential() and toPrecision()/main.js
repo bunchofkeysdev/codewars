a33bfc7f72615f563c2069321b9cecc8b0f9f55a
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-07-13 16:42:44 +0100

    codewars

--- 8kuy/Training JS #15 Methods of Number object--toFixed(), toExponential() and toPrecision()/main.js
+++ 8kuy/Training JS #15 Methods of Number object--toFixed(), toExponential() and toPrecision()/main.js
@@ -2,3 +2,8 @@ function howManySmaller(arr, n) {
     return arr.map(e => e.toFixed(2)).filter(e => e < n).length
 
 }
+
+// second
+function howManySmaller(arr, n) {
+    return arr.filter(x => +x.toFixed(2) < n).length;
+}
