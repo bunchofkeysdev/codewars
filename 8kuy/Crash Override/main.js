08b93170da4d7f6b793cdc18423393414b62c022
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-07-16 14:03:58 +0100

    codewars

--- 8kuy/Crash Override/main.js
+++ 8kuy/Crash Override/main.js
@@ -1,7 +1,10 @@
 function aliasGen(a, b) {
     if (a.charAt(0) == +a.charAt(0) || b.charAt(0) == +b.charAt(0)) {
         return "Your name must start with a letter from A - Z."
-
-
     }
+    a = a.charAt(0).toUpperCase()
+    b = b.charAt(0).toUpperCase()
+
+    return `${firstName[a]} ${surname[b]}`
+
 }
