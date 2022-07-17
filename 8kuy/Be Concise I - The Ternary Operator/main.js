9482b25582e53cd30cee1a8872bf2816fa931b21
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-07-17 13:32:22 +0100

    codewars

--- 8kuy/Be Concise I - The Ternary Operator/main.js
+++ 8kuy/Be Concise I - The Ternary Operator/main.js
@@ -1,1 +1,7 @@
 const describeAge = (age, line = "You're a(n)") => age <= 12 ? line + " kid" : age <= 17 ? line + " teenager" : age <= 64 ? line + " adult" : line + " elderly"
+
+//second
+
+function describeAge(age) {
+    return "You're a(n) " + (age < 13 ? "kid" : age < 18 ? "teenager" : age < 65 ? "adult" : "elderly")
+}
