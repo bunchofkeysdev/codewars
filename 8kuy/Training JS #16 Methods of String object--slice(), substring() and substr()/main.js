cc862952b01592489d41838b73f2b71d48e156d0
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-08-09 18:52:48 +0100

    codewars

--- 8kuy/Training JS #16 Methods of String object--slice(), substring() and substr()/main.js
+++ 8kuy/Training JS #16 Methods of String object--slice(), substring() and substr()/main.js
@@ -4,3 +4,10 @@ function cutIt(arr) {
     return arr.map(e => e.slice(0, small))
 
 }
+
+// second
+
+function cutIt(arr) {
+    const minLength = Math.min(...arr.map(x => x.length));
+    return arr.map(x => x.slice(0, minLength));
+}
