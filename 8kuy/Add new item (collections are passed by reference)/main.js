0df83c7dfc0ca68bb74f75f8b5f23cb953a01360
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-08-13 17:39:43 +0100

    codewars

--- 8kuy/Add new item (collections are passed by reference)/main.js
+++ 8kuy/Add new item (collections are passed by reference)/main.js
@@ -2,3 +2,9 @@ function addExtra(listOfNumbers) {
     let num = [13]
     return listOfNumbers.concat(num)
 }
+
+function addExtra(listOfNumbers) {
+    // your code here
+    // add an int to listOfNumbers and return it
+    return [...listOfNumbers, 13];
+}
