06e9f743e7844db9e881e06305b0eed3bf302501
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-08-13 17:00:32 +0100

    codewars

---
+++ 8kuy/Safen User Input Part I - htmlspecialchars/main.js
@@ -0,0 +1,13 @@
+function htmlspecialchars(formData) {
+    return formData.split('').map(e => {
+        if (e === "<") {
+            return e = `&lt;`
+        } else if (e === '>') {
+            return e = `&gt;`
+        } else if (e === `"`) {
+            return e = `&quot;`
+        } else if (e === `&`) {
+            return e = `&amp;`
+        } else return e
+    }).join('')
+}
