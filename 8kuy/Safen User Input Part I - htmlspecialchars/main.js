8514bd339d07889a1fab4ec5f65bba388917826b
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-08-13 17:18:11 +0100

    codewars

--- 8kuy/Safen User Input Part I - htmlspecialchars/main.js
+++ 8kuy/Safen User Input Part I - htmlspecialchars/main.js
@@ -11,3 +11,12 @@ function htmlspecialchars(formData) {
         } else return e
     }).join('')
 }
+
+//second
+
+function htmlspecialchars(formData) {
+    return formData.replace(/&/g, "&amp;")
+        .replace(/"/g, "&quot;")
+        .replace(/</g, "&lt;")
+        .replace(/>/g, "&gt;");
+}
