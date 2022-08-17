f05f75beeba21b680fbc51d9a753861e2241d297
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-08-17 15:16:06 +0100

    codewars

--- 8kuy/Simple Fun #352: Reagent Formula/main.js
+++ 8kuy/Simple Fun #352: Reagent Formula/main.js
@@ -7,3 +7,9 @@ function isValid(formula) {
     return firstRule && secondRule && thirdRule && fourthRule
 
 }
+
+// shorter
+isValid = a => !(a.includes(1) && a.includes(2)) &&
+    !(a.includes(3) && a.includes(4)) &&
+    a.includes(5) === a.includes(6) &&
+    (a.includes(7) || a.includes(8));
