8816f2b49f35427251c628a5d168a4c97e1b38a7
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-08-19 23:48:41 +0100

    codewars

--- 8kuy/ Training JS #21: Methods of String object--trim() and the string template/main.js
+++ 8kuy/ Training JS #21: Methods of String object--trim() and the string template/main.js
@@ -3,3 +3,16 @@ function fiveLine(s) {
     return `${s}\n${s}${s}\n${s}${s}${s}\n${s}${s}${s}${s}\n${s}${s}${s}${s}${s}`
 
 }
+
+// Better
+function fiveLine(s) {
+    //coding here...
+    s = s.trim();
+    var tmp = s;
+    var res = [s];
+
+    for (var i = 1; i < 5; i++) {
+        res.push(s += tmp);
+    }
+    return res.join("\n");
+}
