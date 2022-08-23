2d730af03c3ffacbfdfa7828ae8995da847f4094
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-08-23 23:58:03 +0100

    codewars

--- 8kuy/Improving Math.round(x)/main.js
+++ 8kuy/Improving Math.round(x)/main.js
@@ -1,3 +1,9 @@
 Math.roundTo = function (number, precision) {
     return precision > number.toString().length ? Math.round(number) : +number.toFixed(precision)
 }
+
+// better
+
+Math.roundTo = function (number, precision) {
+    return +number.toFixed(precision)
+}
