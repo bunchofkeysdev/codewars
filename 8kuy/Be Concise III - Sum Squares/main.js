f9979e1d04bc66cee10815543c7b5da6022f04ab
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-09-01 22:28:55 +0100

    codewars

--- 8kuy/Be Concise III - Sum Squares/main.js
+++ 8kuy/Be Concise III - Sum Squares/main.js
@@ -1,1 +1,15 @@
 const sumSquares = array => array.map(e => e ** 2).reduce((a, b) => b + a)
+
+// longer
+function sumSquares(array) {
+    var result = 0;
+    for (let i = 0; i < array.length; i++) {
+        result += array[i] ** 2;
+    }
+    return result;
+}
+
+// better
+function sumSquares(array) {
+    return array.reduce((a, b) => a + b ** 2, 0);
+}
