d136290d41eaf84b093fd2e6357c688b08a73eb1
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-09-03 21:18:22 +0100

    codewars

--- 7kuy/Remove anchor from URL/main.js
+++ 7kuy/Remove anchor from URL/main.js
@@ -1,3 +1,9 @@
 function removeUrlAnchor(url) {
     return url.includes('#') ? url.slice(0, url.indexOf('#')) : url
 }
+
+// shorter
+
+function removeUrlAnchor(url) {
+    return url.split('#')[0];
+}
