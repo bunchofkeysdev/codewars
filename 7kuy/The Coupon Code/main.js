d51d2684f93c03553acdb6f2b361e9eed6df53e9
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-09-04 14:59:06 +0100

    codewars

--- 7kuy/The Coupon Code/main.js
+++ 7kuy/The Coupon Code/main.js
@@ -1,3 +1,7 @@
 function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
     return (enteredCode === correctCode) && (Date.parse(currentDate) <= Date.parse(expirationDate))
 }
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+    return enteredCode === correctCode && new Date(currentDate) <= new Date(expirationDate);
+}
