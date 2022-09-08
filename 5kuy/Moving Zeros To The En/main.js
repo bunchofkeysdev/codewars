aa565064fc402737aadcdb9f8a771c976d61397b
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-09-08 16:51:42 +0100

    codewars

--- 5kuy/Moving Zeros To The En/main.js
+++ 5kuy/Moving Zeros To The En/main.js
@@ -1,1 +1,9 @@
 const moveZeros = arr => arr.filter(e => e !== 0).concat(arr.filter(e => e === 0))
+
+// second
+var moveZeros2 = function (arr) {
+    return [
+        ...(arr.filter(n => n !== 0)),
+        ...(arr.filter(n => n === 0))
+    ];
+}
