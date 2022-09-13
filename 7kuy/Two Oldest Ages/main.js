95838f0c0870739dd767d3269822b96b89d0741b
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-09-14 00:59:08 +0100

    codewars

--- 7kuy/Two Oldest Ages/main.js
+++ 7kuy/Two Oldest Ages/main.js
@@ -3,3 +3,7 @@ function twoOldestAges(ages){
      return [newAge[1],newAge[0]]
     }
     
+    // second
+    function twoOldestAges(ages){
+        return ages.sort(function(a,b){return a-b;}).slice(-2);
+      }
