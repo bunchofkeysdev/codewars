1d86c89dd918c4ea67e5dd808857edad986c59ce
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-09-29 13:51:07 +0100

    codewars

--- 7kuy/Smallest value of an array/main.js
+++ 7kuy/Smallest value of an array/main.js
@@ -1,3 +1,8 @@
 function min(arr, toReturn) {
     return toReturn === 'index' ? arr.indexOf(Math.min(...arr)) : Math.min(...arr)
 }
+
+function min(arr, toReturn) {
+    let min = Math.min(...arr)
+    return toReturn === 'index' ? arr.indexOf(min) : min
+}
