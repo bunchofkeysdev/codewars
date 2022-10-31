46b0318946b1d7360cd4017092cb2988d5340bc9
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-10-31 23:20:00 +0100

    codewars

--- 6kuy/String transformer/main.js
+++ 6kuy/String transformer/main.js
@@ -1,1 +1,15 @@
 const stringTransformer = (str) => str.split(' ').reverse().map(e => e.split('').map(e => e === e.toLowerCase() ? e.toUpperCase() : e.toLowerCase()).join('')).join(' ')
+
+
+// Better code
+function stringTransformer(str) {
+    return str
+        .split(' ')
+        .reverse()
+        .join(' ')
+        .split('')
+        .map(v => v == v.toUpperCase() ?
+            v.toLowerCase() :
+            v.toUpperCase())
+        .join('');
+}
