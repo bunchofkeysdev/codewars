0b86379a6424980d2d88aa5c92cee43a6def73ae
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-11-11 22:59:03 +0100

    Codewars

---
+++ 7kuy/Search for letters/main.js
@@ -0,0 +1,8 @@
+function change(string) {
+    const stringInLowerCase = string.toLowerCase()
+
+    return 'abcdefghijklmnopqrstuvwxyz'
+        .split('')
+        .map(char => (stringInLowerCase.includes(char) ? 1 : 0))
+        .join('')
+}
