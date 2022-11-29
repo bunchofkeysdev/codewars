4a7ccdb68878f48ccc10f545b034cc2a22a2ed29
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-11-30 00:01:19 +0100

    codewars

--- 7kuy/Evens times last/main.js
+++ 7kuy/Evens times last/main.js
@@ -1,3 +1,6 @@
 function evenLast(numbers) {
     return numbers.length === 0? 0 :numbers.filter((e,i,a)=>i%2==0).reduce((a,b)=>a+b,0)*numbers[numbers.length-1]
   }
+
+//   second
+const evenLast = n => n.reduce((p, c, i) => i % 2 ? p : p + c, 0) * n[n.length - 1] || 0
