e69f69a1b9088ad6db50e4d22861ef01285e107b
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-12-05 00:00:08 +0100

    codewars

--- 7kuy/ Array Leaders (Array Series #3)/main.js
+++ 7kuy/ Array Leaders (Array Series #3)/main.js
@@ -1,3 +1,6 @@
 function arrayLeaders(numbers){
     return numbers.filter((e,i,a)=>e>a.slice(i+1).reduce((a,b)=>a+b,0))
   }
+
+//   second
+const arrayLeaders = (numbers) => numbers.filter((e,i,a)=>e>a.slice(i+1).reduce((a,b)=>a+b,0))
