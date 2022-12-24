19461afd73599a8319d9c8053d926de0c13ef4fc
bunchofkeysdev <bunchofkeysdev@gmail.com>
2022-12-24 16:55:20 +0100

    codewars

---
+++ 8kuy/Configure package json for a node application/main.js
@@ -0,0 +1,13 @@
+const configuration = {
+    "name": "your-pack-name",
+    "description": "",
+  "version" : "1.0.0",
+    "devDependencies": {
+    "babel-core": "^6.0.0",
+    "babel-polyfill": "^6.0.0",
+    "babel-preset-es2015": "^6.0.0",
+    "babel-preset-stage-0": "^6.0.0"
+  }
+  };
+  // Hint: try creating with `npm init` command
+  // Help! -> https://docs.npmjs.com/files/package.json
