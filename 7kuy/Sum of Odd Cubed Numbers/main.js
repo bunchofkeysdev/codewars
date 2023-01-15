758bf5986d4056b97b7e4081d72550b68801ffcc
bunchofkeysdev <bunchofkeysdev@gmail.com>
2023-01-15 10:41:19 +0100

    codewars

--- 7kuy/Sum of Odd Cubed Numbers/main.js
+++ 7kuy/Sum of Odd Cubed Numbers/main.js
@@ -5,3 +5,8 @@ function cubeOdd(arr) {
     
     
     }
+
+    //second
+    const cubeOdd = arr =>
+ arr.every(element => typeof element === 'number')?
+arr.map(e=> e*e*e).filter(e=>e%2).reduce((a,b)=>a+b,0) : undefined
