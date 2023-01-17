b7945c124aba2f89c405d48de966755255e2fdf7
bunchofkeysdev <bunchofkeysdev@gmail.com>
2023-01-18 00:07:33 +0100

    codewars

--- 7kuy/The Office I - Outed/main.js
+++ 7kuy/The Office I - Outed/main.js
@@ -10,3 +10,9 @@ function outed(meet, boss){
   }
    return arr.reduce((a,b)=>a+b,0) /arr.length <= 5 ? "Get Out Now!" : "Nice Work Champ!"
   }
+//   second
+function outed(meet, boss) {
+    let names = Object.keys(meet)
+    let score = names.reduce((s,v) => s + meet[v], 0) + meet[boss]
+    return score / names.length > 5 ? 'Nice Work Champ!' : 'Get Out Now!'
+  }
