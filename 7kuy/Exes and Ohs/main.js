b57e1b0657a14904dc27afd043538d4a19b87fbb
bunchofkeysdev <bunchofkeysdev@gmail.com>
2023-06-07 22:08:51 +0100

    codewars

--- 7kuy/Exes and Ohs/main.js
+++ 7kuy/Exes and Ohs/main.js
@@ -10,3 +10,12 @@ function XO(str) {
   }
   return x.length === o.length;
 }
+// reformatted
+function XO(str) {
+  var sum = 0;
+  for (var i = 0; i < str.length; i++) {
+    if (str[i].toLowerCase() == "x") sum++;
+    if (str[i].toLowerCase() == "o") sum--;
+  }
+  return sum == 0;
+}
