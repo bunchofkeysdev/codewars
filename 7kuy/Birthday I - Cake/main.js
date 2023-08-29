85722b28248ba7820185c45babe8e3e2f7dfc6ea
bunchofkeysdev <bunchofkeysdev@gmail.com>
2023-08-29 17:14:44 +0100

    codewars

--- 7kuy/Birthday I - Cake/main.js
+++ 7kuy/Birthday I - Cake/main.js
@@ -11,3 +11,19 @@ function cake(x, y) {
 
   return sum > (x * 70) / 100 ? "Fire!" : "That was close!";
 }
+
+function cake(x, y) {
+  let sum = 0;
+
+  for (let i = 0; i < y.length; i++) {
+    const charCode = y.charCodeAt(i);
+
+    if (i % 2 == 0) {
+      sum += charCode;
+    } else {
+      sum += charCode - 96;
+    }
+  }
+
+  return sum > (x * 70) / 100 ? "Fire!" : "That was close!";
+}
