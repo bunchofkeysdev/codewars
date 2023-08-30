b6c7a306fcf1f96b1ad0e20aea0256538b8fa74e
bunchofkeysdev <bunchofkeysdev@gmail.com>
2023-08-30 17:14:03 +0100

    codewars

--- 7kuy/Birthday I - Cake/main.js
+++ 7kuy/Birthday I - Cake/main.js
@@ -1,13 +1,11 @@
 function cake(x, y) {
   let sum = 0;
-  let arr = [];
+
   for (let i = 0; i < y.length; i++) {
     if (i % 2 == 0) {
       sum += y.charCodeAt(i);
-      arr.push(y.charCodeAt(i));
     } else {
       sum += y.charCodeAt(i) - 96;
-      arr.push(y.charCodeAt(i) - 96);
     }
   }
 
