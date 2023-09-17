ac802695a6f096b32e91d095af3f787c466863f2
bunchofkeysdev <bunchofkeysdev@gmail.com>
2023-09-17 16:55:42 +0100

    codewars

---
+++ 7kuy/Don't give me five!/main.js
@@ -0,0 +1,7 @@
+function dontGiveMeFive(start, end) {
+  let arr = [];
+  for (let i = start; i <= end; i++) {
+    if (!String(i).split("").includes("5")) arr.push(i);
+  }
+  return arr.length;
+}
