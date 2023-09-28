c7c129ebb6e01f3f13290a0a6ac0572c569d6180
bunchofkeysdev <bunchofkeysdev@gmail.com>
2023-09-28 18:04:02 +0100

    codewars

--- 7kuy/Growth of a Population/main.js
+++ 7kuy/Growth of a Population/main.js
@@ -1,8 +1,7 @@
 function nbYear(p0, percent, aug, p) {
-  let x = 0;
   let n = 0;
-  while (x < p) {
-    x = p0 + p0 * (percent / 100) + aug;
+  while (p0 < p) {
+    p0 = p0 + p0 * (percent / 100) + aug;
     n++;
   }
   return n;
