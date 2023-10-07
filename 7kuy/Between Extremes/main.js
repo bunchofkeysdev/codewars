9e6e5825ed588f4fce230d977be614ec9ae2a6c8
bunchofkeysdev <bunchofkeysdev@gmail.com>
2023-10-07 16:32:09 +0100

    codewars

--- 7kuy/Between Extremes/main.js
+++ 7kuy/Between Extremes/main.js
@@ -3,3 +3,9 @@ function betweenExtremes(numbers) {
   let length = numbers.length - 1;
   return numbers[length] - numbers[0];
 }
+
+function betweenExtremes(numbers) {
+  let maxNum = Math.max(...numbers);
+  let minNum = Math.min(...numbers);
+  return maxNum - minNum;
+}
