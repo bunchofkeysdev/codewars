7e8dfff59db0c83504746a8a899af9c4a0503391
bunchofkeysdev <bunchofkeysdev@gmail.com>
2024-06-17 23:43:48 +0100

    reformatted

--- 8kuy/Merging sorted integer arrays (without duplicates)/main.js
+++ 8kuy/Merging sorted integer arrays (without duplicates)/main.js
@@ -1,3 +1,7 @@
 function mergeArrays(a, b) {
   return Array.from(new Set(a.concat(b))).sort((a, b) => a - b);
 }
+
+// Refactored
+
+const mergeArrays = (a, b) => [...new Set(a.concat(b))].sort((a, b) => a - b);
